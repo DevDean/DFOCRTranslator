51d6f86afe2d0df9cb4cb854de2ab0216a658402
Adrian Dean Sarmiento <adriandean12@gmail.com>
2016-08-21 17:42:21 +0800

    Added the OCR module but cannot test it yet.

--- app/src/main/java/com/deanandfaye/newocrtranslator/MainActivity.java
+++ app/src/main/java/com/deanandfaye/newocrtranslator/MainActivity.java
@@ -1,6 +1,8 @@
 package com.deanandfaye.newocrtranslator;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
@@ -18,12 +20,20 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.wordpress.priyankvex.easyocrscannerdemo.Config;
+import com.wordpress.priyankvex.easyocrscannerdemo.EasyOcrScanner;
+import com.wordpress.priyankvex.easyocrscannerdemo.EasyOcrScannerListener;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,EasyOcrScannerListener {
 
+    EasyOcrScanner mEasyOcrScanner;
+    TextView textView;
+    ProgressDialog mProgressDialog;
     Button button;
     ImageView imageView;
     static final int CAM_REQUEST = 1;
@@ -36,16 +46,22 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        textView = (TextView) findViewById(R.id.textView);
         button = (Button) findViewById(R.id.button);
         imageView = (ImageView) findViewById(R.id.image_view);
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                File file = getFile();
-                camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
-                startActivityForResult(camera_intent,CAM_REQUEST);
 
+        // initialize EasyOcrScanner instance.
+        mEasyOcrScanner = new EasyOcrScanner(MainActivity.this, "EasyOcrScanner",
+                Config.REQUEST_CODE_CAPTURE_IMAGE, "eng");
+
+        // Set ocrScannerListener
+        mEasyOcrScanner.setOcrScannerListener(this);
+
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                mEasyOcrScanner.takePicture();
             }
         });
 
@@ -136,7 +152,30 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        String path = "sdcard/ocr_app/cam_image.jpg";
-        imageView.setImageDrawable(Drawable.createFromPath(path));
+        super.onActivityResult(requestCode, resultCode, data);
+        // Call onImageTaken() in onActivityResult.
+        if (resultCode == RESULT_OK && requestCode == Config.REQUEST_CODE_CAPTURE_IMAGE){
+            mEasyOcrScanner.onImageTaken();
+        }
+    }
+
+    @Override
+    public void onOcrScanStarted(String filePath) {
+        mProgressDialog = new ProgressDialog(MainActivity.this);
+        mProgressDialog.setMessage("Scanning...");
+        mProgressDialog.show();
+    }
+    /**
+     * Callback when scanning is finished.
+     * Good place to hide teh progress dialog.
+     * @param bitmap Bitmap of image that was scanned.
+     * @param recognizedText Scanned text.
+     */
+    @Override
+    public void onOcrScanFinished(Bitmap bitmap, String recognizedText) {
+        textView.setText(recognizedText);
+        if (mProgressDialog.isShowing()){
+            mProgressDialog.dismiss();
+        }
     }
 }
